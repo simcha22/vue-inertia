e149feb515b600407f305fb35a1af7bfeae832da
simcha fridman <simchafridman@simchas-MacBook-Air.local>
2023-08-14 15:14:37 +0300

    roles and permmisions lesson 02

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\RoleName;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +44,47 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->hasRole(RoleName::ADMIN);
+    }
+
+    public function isVendor(): bool
+    {
+        return $this->hasRole(RoleName::VENDOR);
+    }
+
+    public function isStaff(): bool
+    {
+        return $this->hasRole(RoleName::STAFF);
+    }
+
+    public function isCustomer(): bool
+    {
+        return $this->hasRole(RoleName::CUSTOMER);
+    }
+
+    public function hasRole(RoleName $role): bool
+    {
+        return $this->roles()->where('name', $role->value)->exists();
+    }
+
+    public function permissions(): array
+    {
+        return $this->roles()->with('permissions')->get()
+            ->map(function ($role) {
+                return $role->permissions->pluck('name');
+            })->flatten()->values()->unique()->toArray();
+    }
+
+    public function hasPermission(string $permission): bool
+    {
+        return in_array($permission, $this->permissions(), true);
+    }
 }
