d2b64a18b5770c22658e3a3a64061d55c417137e
simcha fridman <simchafridman@simchas-MacBook-Air.local>
2023-08-16 14:15:44 +0300

    lesson 05

--- resources/js/Components/TextareaInput.vue
+++ resources/js/Components/TextareaInput.vue
@@ -22,10 +22,10 @@ defineExpose({focus: () => input.value.focus()})
 </script>
 
 <template>
-    <input
-        class="border-gray-300 focus:border-primary-500 focus:ring-primary-500 rounded-md shadow-sm"
-        :value="modelValue"
-        @input="$emit('update:modelValue', $event.target.value)"
-        ref="input"
-    />
+  <textarea
+      class="border-gray-300 focus:border-primary-500 focus:ring-primary-500 rounded-md shadow-sm"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      ref="input"
+  ></textarea>
 </template>
