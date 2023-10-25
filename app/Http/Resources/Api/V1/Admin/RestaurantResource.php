76c39d89f0b4a19866dd91c3a7b8f4a613f3964c
simcha fridman <simchafridman@simchas-MacBook-Air.local>
2023-10-25 16:31:19 +0300

    api course lesson 01

---
+++ app/Http/Resources/Api/V1/Admin/RestaurantResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\Api\V1\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RestaurantResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'   => $this->id,
+            'name' => $this->name,
+        ];
+    }
+}
